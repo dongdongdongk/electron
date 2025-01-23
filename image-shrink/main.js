616b1bdecd7680056925b7a88048ad6785d1c65e
dongdongdongk <dhk9309@gmail.com>
2025-01-23 17:35:51 +0900

    기본 윈도우 만들기 && 아이콘

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,18 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow
+
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: 500,
         height: 600,
+        icon: './assets/Icon_256x256.png'
     })
+    // mainWindow.loadURL('https://www.base64decode.org/')
+    // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+    mainWindow.loadFile('./app/index.html')
 }
+
 
 app.on('ready', createMainWindow);
