6d5c9a83e89b0371138ccf3520d5faa362806734
dongdongdongk <dhk9309@gmail.com>
2025-02-13 13:31:42 +0900

    사용자 데이터 저장 가져오기 버그 수정정

--- sys-top/app/js/monitor.js
+++ sys-top/app/js/monitor.js
@@ -1,5 +1,5 @@
-let cpuOverload = 15;
-let alertFrequency = 1;
+let cpuOverload
+let alertFrequency 
 let firstRun = true; // ✅ 첫 실행 여부를 확인하는 변수 추가
 
 // Set model
@@ -82,4 +82,39 @@ function runNotify(frequency) {
   } else {
     return false
   }
+}
+
+
+
+//렌더러 js
+
+window.electron.on("settings:get", (settings) => {
+  console.log("설정 데이터:", settings);
+  document.getElementById('cpu-overload').value = settings.cpuOverload
+  document.getElementById('alert-frequency').value = settings.alertFrequency
+  cpuOverload = settings.cpuOverload
+  alertFrequency = settings.alertFrequency
+});
+
+
+const settingsForm = document.getElementById("settings-form"); // 폼 요소 선택
+settingsForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const cpuOverload = document.getElementById('cpu-overload').value
+  const alertFrequency = document.getElementById('alert-frequency').value
+
+  // 메인 프로세스로 데이터 전송
+  window.electron.send("settings:set", { cpuOverload, alertFrequency });
+  showAlert('Setting Saved')
+})
+
+function showAlert(msg) {
+  const alert = document.getElementById('alert');
+  alert.classList.remove('hide')
+  alert.classList.add('alert')
+  alert.innerText = msg
+
+  setTimeout(() => {
+      alert.classList.add('hide')
+  },3000)
 }
